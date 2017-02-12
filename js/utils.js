8cf89f38c53a7e2edb85149d5922637c7e271ebf
a.masalov <delsol@bk.ru>
2017-02-12 11:25:19 +0300

    isolate modules

--- js/utils.js
+++ js/utils.js
@@ -1,18 +1,38 @@
 'use strict';
 
-window.utils = {
-  KEY_CODES: {
-    'escape': 27,
-    'enter': 13
-  },
-  getRandomElement: function (arr) {
+window.utils = (function () {
+  var KEY_CODES = {
+    escape: 27,
+    enter: 13
+  };
+
+  var isKeyboardEvent = function (evt) {
+    return typeof evt.keyCode !== 'undefined';
+  };
+
+  function getRandomElement(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
-  },
-  getRandomElementExcept: function (arr, item) {
-    var currentValue = null;
-    do {
-      currentValue = this.getRandomElement(arr);
-    } while (currentValue === item);
-    return currentValue;
   }
-};
+
+  return {
+    isDeactivationEvent: function (evt) {
+      return isKeyboardEvent(evt) && evt.keyCode === KEY_CODES.escape;
+    },
+
+    isActivationEvent: function (evt) {
+      return isKeyboardEvent(evt) && evt.keyCode === KEY_CODES.enter || evt.type === 'click';
+    },
+
+    getRandomElement: getRandomElement,
+
+    getRandomElementExcept: function (arr, item) {
+      var currentValue = null;
+
+      do {
+        currentValue = getRandomElement(arr);
+      } while (currentValue === item);
+
+      return currentValue;
+    }
+  };
+})();
